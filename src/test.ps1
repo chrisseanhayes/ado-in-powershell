f4aad0410f24e7fb6c58398f2681aa1b34d6928a
Chris Hayes <hayes.cs@gmail.com>
2020-08-22 12:37:05 -0700

    video2 stopping point

--- src/test.ps1
+++ src/test.ps1
@@ -1,22 +1,57 @@
-$client = new-object System.Net.Http.HttpClient
-#accept header for json
-$client.DefaultRequestHeaders.Accept.Clear()
-$acceptjson = new-object System.Net.Http.Headers.MediaTypeWithQualityHeaderValue -ArgumentList "application/json"
-$client.DefaultRequestHeaders.Accept.Add($acceptjson)
+function convert-to-base64([string]$value){
+    $base64val = [Convert]::ToBase64String([System.Text.ASCIIEncoding]::ASCII.GetBytes($value))
+    return $base64val
+}
 
-#auth header
-$pat = $env:PAT
-$base64authheader = [Convert]::ToBase64String([System.Text.ASCIIEncoding]::ASCII.GetBytes(":$pat"))
-$auth = new-object System.Net.Http.Headers.AuthenticationHeaderValue -ArgumentList ("Basic", $base64authheader)
-$client.DefaultRequestHeaders.Authorization = $auth
+function get-pat-auth-header($pat){
+    $base64authheader = convert-to-base64 ":$pat"
+    $auth = new-object System.Net.Http.Headers.AuthenticationHeaderValue -ArgumentList ("Basic", $base64authheader)
+    return $auth
+}
 
-#url formatting
+function get-json-accept-header{
+    return new-object System.Net.Http.Headers.MediaTypeWithQualityHeaderValue -ArgumentList "application/json"
+}
+
+function get-ado-web-client{
+    $client = new-object System.Net.Http.HttpClient
+    #accept header for json
+    $client.DefaultRequestHeaders.Accept.Clear()
+    $client.DefaultRequestHeaders.Accept.Add($(get-json-accept-header))
+    
+    #auth header
+    $client.DefaultRequestHeaders.Authorization = get-pat-auth-header $env:PAT
+    return $client
+}
+$currentversion = "5.1"
 $organization = "chrisseanhayes"
-$url = "https://dev.azure.com/$organization/_apis/projects?api-version=5.1"
+$getadoprojecturlwithversion = {
+    param([string]$version, [string]$organization) `
+        return {
+                return "https://dev.azure.com/$organization/_apis/projects?api-version=$version"
+        }.GetNewClosure()
+    }
+$getadoprojecturl = & $getadoprojecturlwithversion $currentversion $organization
+$function:getprojecturl = $getadoprojecturl
 
-#request
-$response = $client.GetAsync($url).Result
+$getadoobjectwithclient = {
+    param($client) `
+        return {
+            return { param($url) {
+                $response = $client.GetAsync($url).Result
 
-$result = $response.Content.ReadAsStringAsync().Result
+                $result = $response.Content.ReadAsStringAsync().Result
+                
+                $object = ConvertFrom-Json $result
+                
+                return $object
+            }}.GetNewClosure()
+        }
+}
+$client = get-ado-web-client
+$getadoobject = & $getadoobjectwithclient $client
+$function:getadoobject = $getadoobject
 
-$result
+$project = getadoobject getprojecturl
+
+$project
